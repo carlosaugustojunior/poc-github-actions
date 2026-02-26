96537c128ee36a7a4d57a11d4ff395fe3e35149a
Carlos Junior <carlos.augusto@ambevtech.com.br>
2026-02-26 15:33:37 -0300

    Initial commit

---
+++ src/PocGithubActions/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PocGithubActions.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
